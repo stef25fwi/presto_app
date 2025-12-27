8bae0f6fca370219c59ba3e8144b77e774eecd4d
stef25fwi <sahai.stephane@gmail.com>
2025-12-27 20:15:14 +0000

    feat(mobile): ajouter Firebase Crashlytics

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
     // Google Services plugin for Firebase
     id("com.google.gms.google-services")
+    // Firebase Crashlytics
+    id("com.google.firebase.crashlytics")
 }
 
 android {
