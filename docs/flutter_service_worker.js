bce72b63be4f96b85c09da8d3ceec47b2077705a
Stephane Sahai <sahai.stephane@gmail.com>
2026-01-13 23:27:34 +0000

    deploy: update GitHub Pages (docs)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -159,7 +159,7 @@ const RESOURCES = {"assets/assets/images/comment_ca_marche_4K.png": "1aeea4d7169
 "index.html": "c229c5ae58039081cf9ebfa5ddc8b779",
 "/": "c229c5ae58039081cf9ebfa5ddc8b779",
 "main.dart.js": "833f08b0d5113e3fa072a4b7f551d7b7",
-"flutter_bootstrap.js": "ab8d9957ed918ea8453bb997aa931ede",
+"flutter_bootstrap.js": "b9165d550d234b804e817024f91aaa19",
 "version.json": "8df3cd3c7c223f3ebda8fbb9cdf0967f",
 "manifest.json": "b8e012f650576f16cebd94a9a9003fb4",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e"};
