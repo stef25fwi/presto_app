2cfe54997f6ab4ac92a5861518caec940dc3781d
Stephane Sahai <sahai.stephane@gmail.com>
2026-01-14 02:13:37 +0000

    deploy: update GitHub Pages (docs)

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "170656223"
+    serviceWorkerVersion: "331608312"
   }
 });
