382a81bb89911dc1a43e28b8914f0b30a75a4b80
Stephane Sahai <sahai.stephane@gmail.com>
2026-04-06 18:39:40 +0000

    fix(publish): fallback when web mic audio decode fails

--- lib/features/micro_ia/web_audio_recorder_stub.dart
+++ lib/features/micro_ia/web_audio_recorder_stub.dart
@@ -2,6 +2,20 @@
 
 import 'dart:typed_data';
 
+class WebMicroIaAudioUpload {
+  const WebMicroIaAudioUpload({
+    required this.bytes,
+    required this.contentType,
+    required this.extension,
+    required this.usedClientSideWavConversion,
+  });
+
+  final Uint8List bytes;
+  final String contentType;
+  final String extension;
+  final bool usedClientSideWavConversion;
+}
+
 class WebAudioRecorder {
   Future<void> start() {
     throw UnsupportedError('WebAudioRecorder is only available on Web');
@@ -20,4 +34,8 @@ Future<Uint8List> webBlobToBytes(Object blob) {
 /// Stub: not available outside Web.
 Future<Uint8List> webBlobToWav16kMono(Object blob) {
   throw UnsupportedError('webBlobToWav16kMono is only available on Web');
+}
+
+Future<WebMicroIaAudioUpload> webBlobToMicroIaUpload(Object blob) {
+  throw UnsupportedError('webBlobToMicroIaUpload is only available on Web');
 }
