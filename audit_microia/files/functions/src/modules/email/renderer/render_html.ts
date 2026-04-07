75673c0cc1f0d149170bde48ebca88fc153dd540
Stephane Sahai <sahai.stephane@gmail.com>
2026-04-07 12:10:57 +0000

    chore(repo): stop tracking audit_microia exports

--- audit_microia/files/functions/src/modules/email/renderer/render_html.ts
+++
@@ -1,8 +0,0 @@
-export function renderHtml(templateHtml: string, data: Record<string, unknown>): string {
-  let html = templateHtml;
-  for (const [key, value] of Object.entries(data)) {
-    const safeValue = String(value ?? "");
-    html = html.replaceAll(`{{${key}}}`, safeValue);
-  }
-  return html;
-}
