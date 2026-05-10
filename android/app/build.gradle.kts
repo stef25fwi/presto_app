7162c06d20aba0714eef8465688f2dae603efb3e
Stephane Sahai <sahai.stephane@gmail.com>
2026-05-10 20:03:15 +0000

    chore(android): align android configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -69,4 +70,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
