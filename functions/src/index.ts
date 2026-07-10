b28a28f6bc2b9fb14bd5fe90a2b507374f0b12c4
stef25fwi <sahai.stephane@gmail.com>
2026-07-10 00:26:13 -0400

    security: exporte la suppression de compte serveur

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,6 +9,7 @@ setGlobalOptions({
 export { onUserCreated, onUserUpdated } from "./modules/auth/triggers";
 export { onAuthUserCreated } from "./modules/auth/on_auth_user_created";
 export { onUserRolesChanged } from "./modules/auth/role_claims_sync";
+export { requestAccountDeletion } from "./modules/auth/account_deletion";
 export {
   placesAutocomplete,
   placesDetails,
