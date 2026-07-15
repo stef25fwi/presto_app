f35f6b7b293d239bf424412c8fb385875d2b7865
stef25fwi <sahai.stephane@gmail.com>
2026-07-15 17:43:03 -0400

    chore: retirer le helper temporaire inutilisé

--- lib/widgets/account_section_border.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Bordure commune des tuiles principales de la page Mon compte.
-const BorderSide accountSectionBorderSide = BorderSide(
-  color: Color(0xFFB8BEC7),
-  width: 2,
-);
