408084156a28b4d3b1faf45e48d6c275015512f3
stef25fwi <sahai.stephane@gmail.com>
2026-07-15 18:35:28 -0400

    style(test): format subscription return history coverage

--- test/features/subscriptions/subscription_return_history_test.dart
+++ test/features/subscriptions/subscription_return_history_test.dart
@@ -2,8 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:presto_app/features/subscriptions/subscription_return_history.dart';
 
 void main() {
-  test('prepareSubscriptionReturnHistory reste sans effet sur les plateformes non web', () {
-    expect(prepareSubscriptionReturnHistory, returnsNormally);
-    expect(prepareSubscriptionReturnHistory, returnsNormally);
-  });
+  test(
+    'prepareSubscriptionReturnHistory reste sans effet hors Web',
+    () {
+      expect(prepareSubscriptionReturnHistory, returnsNormally);
+      expect(prepareSubscriptionReturnHistory, returnsNormally);
+    },
+  );
 }
