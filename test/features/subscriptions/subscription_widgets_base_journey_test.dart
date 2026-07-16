cc9e9091ea192c70fb4e755b4d9ad812ffe481e8
stef25fwi <sahai.stephane@gmail.com>
2026-07-16 12:09:41 -0400

    fix(subscriptions): afficher Gratuit Pro dans les offres (#490)
    Ajoute Gratuit Pro à l’onglet professionnel, conserve les offres particuliers, complète les tests et retire les workflows temporaires de réparation.

--- test/features/subscriptions/subscription_widgets_base_journey_test.dart
+++ test/features/subscriptions/subscription_widgets_base_journey_test.dart
@@ -108,6 +108,9 @@ void main() {
     await tester.tap(proSelector);
     await tester.pumpAndSettle();
 
+    expect(find.text('Gratuit Pro'), findsOneWidget);
+    expect(find.text('0 €/mois'), findsNWidgets(2));
+    expect(find.text('ilipro'), findsOneWidget);
     expect(find.text('Choisir ilipro'), findsOneWidget);
     expect(find.text('Choisir iliprestō+'), findsNothing);
 
