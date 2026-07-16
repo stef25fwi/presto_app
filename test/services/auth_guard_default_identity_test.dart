1b9e653a57093ebe96b162ec1bb4a8a5f365caed
stef25fwi <sahai.stephane@gmail.com>
2026-07-16 01:03:05 -0400

    test(auth): supprimer l’import inutilisé du garde

--- test/services/auth_guard_default_identity_test.dart
+++ test/services/auth_guard_default_identity_test.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/test.dart';
