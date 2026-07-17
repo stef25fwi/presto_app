3a045ba2fd571315bbf64858037a7d1df223e654
stef25fwi <sahai.stephane@gmail.com>
2026-07-17 16:48:14 -0400

    fix(auth): retirer l'import Flutter inutile

--- lib/pages/account/delete_account_page.dart
+++ lib/pages/account/delete_account_page.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import '../../services/auth_error_mapper.dart';
