6e8a09db622610d0f68abf4b411fa17d7538837a
stef25fwi <sahai.stephane@gmail.com>
2026-07-19 10:07:45 -0400

    test(payments): couvrir les délégations du contrôleur audio

---
+++ test/widgets/payment_info_audio_controller_delegation_test.dart
@@ -0,0 +1,63 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:presto_app/widgets/payment_info_audio_player_button.dart';
+
+class _RecordingAudioPlayer extends AudioPlayer {
+  final calls = <String>[];
+  Source? receivedSource;
+  Duration? receivedPosition;
+
+  @override
+  Future<void> setSource(Source source) async {
+    receivedSource = source;
+    calls.add('setSource');
+  }
+
+  @override
+  Future<void> stop() async {
+    calls.add('stop');
+  }
+
+  @override
+  Future<void> pause() async {
+    calls.add('pause');
+  }
+
+  @override
+  Future<void> resume() async {
+    calls.add('resume');
+  }
+
+  @override
+  Future<void> seek(Duration position) async {
+    receivedPosition = position;
+    calls.add('seek');
+  }
+}
+
+void main() {
+  test('délègue les commandes de lecture au lecteur Audioplayers', () async {
+    final player = _RecordingAudioPlayer();
+    final controller = AudioplayersPaymentInfoAudioController(player: player);
+
+    await controller.setSource('https://cdn.test/payment-info.mp3');
+    await controller.stop();
+    await controller.pause();
+    await controller.resume();
+    await controller.seek(const Duration(milliseconds: 750));
+
+    expect(player.calls, <String>[
+      'setSource',
+      'stop',
+      'pause',
+      'resume',
+      'seek',
+    ]);
+    expect(player.receivedSource, isA<UrlSource>());
+    expect(
+      (player.receivedSource! as UrlSource).url,
+      'https://cdn.test/payment-info.mp3',
+    );
+    expect(player.receivedPosition, const Duration(milliseconds: 750));
+  });
+}
