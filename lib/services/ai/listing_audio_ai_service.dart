925692e48418f17e666b8bc8f403ea9897a43d05
stef25fwi <sahai.stephane@gmail.com>
2026-07-19 12:59:48 -0400

    test(publication): couvrir le service audio IA des annonces (#574)
    * refactor(publication): injecter les dépendances du service audio IA
    
    * test(publication): couvrir le service audio IA
    
    * fix(publication): supprimer l import redondant du service audio IA

--- lib/services/ai/listing_audio_ai_service.dart
+++ lib/services/ai/listing_audio_ai_service.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-import 'dart:typed_data';
 
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 
 import '../../config/env/openai_config.dart';
 import '../../features/micro_ia/micro_ia_service.dart';
@@ -12,6 +12,21 @@ import '../../models/ai/listing_ai_result.dart';
 import '../../services/firebase_functions_region.dart';
 import '../../utils/retry.dart';
 import 'listing_ai_mapper.dart';
+
+typedef ListingAudioSecureUploadPreparer = Future<void> Function({
+  required bool forceRefreshToken,
+  required bool forceRefreshAppCheckToken,
+});
+typedef ListingAudioStorageWriter = Future<void> Function({
+  required String storagePath,
+  required Uint8List audioBytes,
+  required String contentType,
+});
+typedef ListingAudioCallableInvoker = Future<Object?> Function({
+  required String name,
+  required Duration timeout,
+  required Map<String, dynamic> parameters,
+});
 
 Map<String, dynamic> _asMap(Object? value) {
   if (value is Map<String, dynamic>) return value;
@@ -34,17 +49,41 @@ class ListingAudioAiService {
   ListingAudioAiService({
     FirebaseFunctions? functions,
     FirebaseStorage? storage,
+    ListingAudioSecureUploadPreparer? secureUploadPreparer,
+    ListingAudioStorageWriter? storageWriter,
+    ListingAudioCallableInvoker? callableInvoker,
   })  : _injectedFunctions = functions,
-        _injectedStorage = storage;
+        _injectedStorage = storage,
+        _secureUploadPreparer = secureUploadPreparer,
+        _storageWriter = storageWriter,
+        _callableInvoker = callableInvoker;
 
   final FirebaseFunctions? _injectedFunctions;
   final FirebaseStorage? _injectedStorage;
+  final ListingAudioSecureUploadPreparer? _secureUploadPreparer;
+  final ListingAudioStorageWriter? _storageWriter;
+  final ListingAudioCallableInvoker? _callableInvoker;
 
   FirebaseFunctions get _functions =>
       _injectedFunctions ?? prestoFirebaseFunctions;
   FirebaseStorage get _storage => _injectedStorage ?? FirebaseStorage.instance;
 
-  bool _isRetryableStorageWriteError(Object error) {
+  @visibleForTesting
+  static Map<String, dynamic> asMapForTest(Object? value) => _asMap(value);
+
+  @visibleForTesting
+  static bool shouldRetryCallableForTest(Object error) =>
+      _shouldRetryCallable(error);
+
+  @visibleForTesting
+  static bool isRetryableStorageWriteErrorForTest(Object error) =>
+      _isRetryableStorageWriteError(error);
+
+  @visibleForTesting
+  static bool isStorageAuthErrorForTest(Object error) =>
+      _isStorageAuthError(error);
+
+  static bool _isRetryableStorageWriteError(Object error) {
     if (error is TimeoutException) return true;
     if (error is FirebaseException) {
       return error.code == 'network-error' ||
@@ -54,7 +93,7 @@ class ListingAudioAiService {
     return false;
   }
 
-  bool _isStorageAuthError(Object error) {
+  static bool _isStorageAuthError(Object error) {
     if (error is FirebaseException) {
       return error.code == 'unauthorized' ||
           error.code == 'permission-denied' ||
@@ -67,6 +106,15 @@ class ListingAudioAiService {
     bool forceRefreshToken = false,
     bool forceRefreshAppCheckToken = false,
   }) async {
+    final override = _secureUploadPreparer;
+    if (override != null) {
+      await override(
+        forceRefreshToken: forceRefreshToken,
+        forceRefreshAppCheckToken: forceRefreshAppCheckToken,
+      );
+      return;
+    }
+
     final secureContext = await MicroIaService.prepareSecureCallableContext(
       forceRefreshToken: forceRefreshToken,
       forceRefreshAppCheckToken: forceRefreshAppCheckToken,
@@ -86,6 +134,52 @@ class ListingAudioAiService {
         );
       }
     }
+  }
+
+  Future<void> _writeStorage({
+    required String storagePath,
+    required Uint8List audioBytes,
+    required String contentType,
+  }) async {
+    final writer = _storageWriter;
+    if (writer != null) {
+      await writer(
+        storagePath: storagePath,
+        audioBytes: audioBytes,
+        contentType: contentType,
+      ).timeout(OpenAiConfig.uploadTimeout);
+      return;
+    }
+
+    final ref = _storage.ref(storagePath);
+    await ref
+        .putData(
+          audioBytes,
+          SettableMetadata(contentType: contentType),
+        )
+        .timeout(OpenAiConfig.uploadTimeout);
+  }
+
+  Future<Object?> _invokeCallable({
+    required String name,
+    required Duration timeout,
+    required Map<String, dynamic> parameters,
+  }) async {
+    final override = _callableInvoker;
+    if (override != null) {
+      return override(
+        name: name,
+        timeout: timeout,
+        parameters: parameters,
+      );
+    }
+
+    final callable = _functions.httpsCallable(
+      name,
+      options: HttpsCallableOptions(timeout: timeout),
+    );
+    final response = await callable.call<dynamic>(parameters);
+    return response.data;
   }
 
   Future<String> uploadAudioBytes({
@@ -111,18 +205,16 @@ class ListingAudioAiService {
     final storagePath = storagePrefix == 'stt_streaming'
         ? '$storagePrefix/$ownerUid/${timestamp}_chunk.$normalizedExtension'
         : '$storagePrefix/${ownerUid}_$timestamp.$normalizedExtension';
-    final ref = _storage.ref(storagePath);
 
     await _prepareProtectedStorageUpload();
 
     Future<void> uploadOnce() {
-      return retry(
-        () => ref
-            .putData(
-              audioBytes,
-              SettableMetadata(contentType: normalizedContentType),
-            )
-            .timeout(OpenAiConfig.uploadTimeout),
+      return retry<void>(
+        () => _writeStorage(
+          storagePath: storagePath,
+          audioBytes: audioBytes,
+          contentType: normalizedContentType,
+        ),
         maxAttempts: 3,
         retryIf: _isRetryableStorageWriteError,
       );
@@ -149,21 +241,20 @@ class ListingAudioAiService {
     required String storagePath,
     String languageCode = OpenAiConfig.defaultLanguageCode,
   }) async {
-    final callable = _functions.httpsCallable(
-      OpenAiConfig.transcribeListingAudioCallable,
-      options: HttpsCallableOptions(timeout: OpenAiConfig.audioTimeout),
-    );
-
-    final response = await retry(
-      () => callable.call<dynamic>(<String, dynamic>{
-        'storagePath': storagePath,
-        'languageCode': languageCode,
-      }),
+    final responseData = await retry<Object?>(
+      () => _invokeCallable(
+        name: OpenAiConfig.transcribeListingAudioCallable,
+        timeout: OpenAiConfig.audioTimeout,
+        parameters: <String, dynamic>{
+          'storagePath': storagePath,
+          'languageCode': languageCode,
+        },
+      ),
       maxAttempts: 2,
       retryIf: _shouldRetryCallable,
     );
 
-    final data = _asMap(response.data);
+    final data = _asMap(responseData);
     final transcription = _asMap(data['transcription']);
     return AudioTranscriptionResult.fromMap(transcription);
   }
@@ -172,21 +263,20 @@ class ListingAudioAiService {
     required String storagePath,
     required ListingAiRequest request,
   }) async {
-    final callable = _functions.httpsCallable(
-      OpenAiConfig.extractListingFieldsFromAudioCallable,
-      options: HttpsCallableOptions(timeout: OpenAiConfig.audioTimeout),
-    );
-
-    final response = await retry(
-      () => callable.call<dynamic>(<String, dynamic>{
-        'storagePath': storagePath,
-        ...request.toCallablePayload(),
-      }),
+    final responseData = await retry<Object?>(
+      () => _invokeCallable(
+        name: OpenAiConfig.extractListingFieldsFromAudioCallable,
+        timeout: OpenAiConfig.audioTimeout,
+        parameters: <String, dynamic>{
+          'storagePath': storagePath,
+          ...request.toCallablePayload(),
+        },
+      ),
       maxAttempts: 2,
       retryIf: _shouldRetryCallable,
     );
 
-    final data = _asMap(response.data);
+    final data = _asMap(responseData);
     return ListingAiMapper.fromAudioCallableResponse(data);
   }
 
