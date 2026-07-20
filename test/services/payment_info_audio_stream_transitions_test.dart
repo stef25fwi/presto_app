39db3330281bcb5a117b8a25fa759ed5517dbb08
stef25fwi <sahai.stephane@gmail.com>
2026-07-19 21:06:43 -0400

    test(coverage): import Firestore SetOptions

--- test/services/payment_info_audio_stream_transitions_test.dart
+++ test/services/payment_info_audio_stream_transitions_test.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:presto_app/services/payment_info_audio_service.dart';
