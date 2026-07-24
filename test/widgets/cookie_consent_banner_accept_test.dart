40356e78a5d61c99c5598a760515349c70a6572c
stef25fwi <sahai.stephane@gmail.com>
2026-07-24 14:52:58 -0400

    test(coverage): stabilize cookie consent accept scenario

--- test/widgets/cookie_consent_banner_accept_test.dart
+++ test/widgets/cookie_consent_banner_accept_test.dart
@@ -10,8 +10,6 @@ void main() {
   testWidgets('accepter masque la bannière et autorise tous les traceurs', (
     tester,
   ) async {
-    await tester.binding.setSurfaceSize(const Size(390, 844));
-    addTearDown(() => tester.binding.setSurfaceSize(null));
     CookieConsentService.instance.resetForTesting();
     SharedPreferences.setMockInitialValues(<String, Object>{});
     await CookieConsentService.instance.load();
