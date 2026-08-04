97c49e0ed33b0eb3ab6cbecedc9da032cb367804
stef25fwi <sahai.stephane@gmail.com>
2026-08-04 13:43:10 -0400

    testability(connectivity): injecter les sources réseau

--- lib/core/connectivity/connectivity_status.dart
+++ lib/core/connectivity/connectivity_status.dart
@@ -3,15 +3,35 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/foundation.dart';
 
+typedef ConnectivityCheck = Future<List<ConnectivityResult>> Function();
+typedef ConnectivityChanges = Stream<List<ConnectivityResult>> Function();
+
 /// Source de vérité unique pour l'état réseau de l'app, utilisée pour
 /// piloter la bannière hors-ligne globale et les gardes d'action réseau.
 class ConnectivityStatus extends ChangeNotifier {
-  ConnectivityStatus._({Connectivity? connectivity})
-      : _connectivity = connectivity ?? Connectivity();
+  ConnectivityStatus._({
+    ConnectivityCheck? checkConnectivity,
+    ConnectivityChanges? connectivityChanges,
+  })  : _checkConnectivity =
+            checkConnectivity ?? Connectivity().checkConnectivity,
+        _connectivityChanges = connectivityChanges ??
+            (() => Connectivity().onConnectivityChanged);
+
+  @visibleForTesting
+  factory ConnectivityStatus.forTesting({
+    required ConnectivityCheck checkConnectivity,
+    required ConnectivityChanges connectivityChanges,
+  }) {
+    return ConnectivityStatus._(
+      checkConnectivity: checkConnectivity,
+      connectivityChanges: connectivityChanges,
+    );
+  }
 
   static final ConnectivityStatus instance = ConnectivityStatus._();
 
-  final Connectivity _connectivity;
+  final ConnectivityCheck _checkConnectivity;
+  final ConnectivityChanges _connectivityChanges;
   StreamSubscription<List<ConnectivityResult>>? _subscription;
   bool _isOnline = true;
   bool _started = false;
@@ -23,13 +43,13 @@ class ConnectivityStatus extends ChangeNotifier {
   void start() {
     if (_started) return;
     _started = true;
-    _subscription = _connectivity.onConnectivityChanged.listen(_handle);
+    _subscription = _connectivityChanges().listen(_handle);
     unawaited(_checkInitialState());
   }
 
   Future<void> _checkInitialState() async {
     try {
-      _handle(await _connectivity.checkConnectivity());
+      _handle(await _checkConnectivity());
     } catch (_) {
       // On garde l'état "en ligne" par défaut si la vérification échoue,
       // pour ne pas afficher une fausse alerte au démarrage.
