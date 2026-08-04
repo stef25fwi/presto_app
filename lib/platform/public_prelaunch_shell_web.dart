80bd44e115f543ff4771007194a5f95490530511
stef25fwi <sahai.stephane@gmail.com>
2026-08-04 00:19:51 -0400

    fix(prelaunch): use a stable JS interop bridge

--- lib/platform/public_prelaunch_shell_web.dart
+++ lib/platform/public_prelaunch_shell_web.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
-import 'package:web/web.dart' as web;
+import 'dart:js_interop';
+
+@JS('iliprestoOpenApplication')
+external void _iliprestoOpenApplication();
 
 void revealApplicationAfterPublicPrelaunch() {
-  final callback = web.window['iliprestoOpenApplication'];
-  if (callback is web.JSFunction) {
-    callback.callAsFunction();
-  }
+  _iliprestoOpenApplication();
 }
