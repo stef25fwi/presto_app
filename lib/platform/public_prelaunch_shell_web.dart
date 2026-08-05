44deb1eb4245870c192fa0d85c579a528c36e59a
stef25fwi <sahai.stephane@gmail.com>
2026-08-05 15:50:12 -0400

    perf(web): transmettre le déverrouillage HTML au démarrage Flutter

--- lib/platform/public_prelaunch_shell_web.dart
+++ lib/platform/public_prelaunch_shell_web.dart
@@ -5,6 +5,17 @@ import 'dart:js_interop';
 @JS('iliprestoOpenApplication')
 external void _iliprestoOpenApplication();
 
+@JS('iliprestoHasPrelaunchAccess')
+external JSBoolean _iliprestoHasPrelaunchAccess();
+
+bool hasPublicPrelaunchAccess() {
+  try {
+    return _iliprestoHasPrelaunchAccess().toDart;
+  } catch (_) {
+    return false;
+  }
+}
+
 void revealApplicationAfterPublicPrelaunch() {
   _iliprestoOpenApplication();
 }
