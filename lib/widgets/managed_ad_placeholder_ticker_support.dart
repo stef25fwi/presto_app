d51b4bf41186407c4873125a0fea88f283a7b42f
stef25fwi <sahai.stephane@gmail.com>
2026-08-06 11:42:09 -0400

    test(coverage): couvrir les assets de repli des bannières (#1251)
    * test(coverage): injecter le manifeste des bannières gérées
    
    * test(coverage): couvrir le chargement des assets de repli

--- lib/widgets/managed_ad_placeholder_ticker_support.dart
+++ lib/widgets/managed_ad_placeholder_ticker_support.dart
@@ -7,12 +7,17 @@ typedef AdPlaceholderImagesWatcher = Stream<List<AdPlaceholderImage>> Function({
 });
 typedef FallbackAssetLoader = Future<List<String>> Function(String prefix);
 typedef BannerImageProviderBuilder = ImageProvider Function(String source);
+typedef AssetManifestEntriesLoader = Future<List<String>> Function();
 
-Future<List<String>> loadManagedAdFallbackAssets(String prefix) async {
-  final manifest = await AssetManifest.loadFromAssetBundle(rootBundle);
+Future<List<String>> loadManagedAdFallbackAssets(
+  String prefix, {
+  AssetManifestEntriesLoader? manifestEntriesLoader,
+}) async {
+  final assets = manifestEntriesLoader != null
+      ? await manifestEntriesLoader()
+      : (await AssetManifest.loadFromAssetBundle(rootBundle)).listAssets();
   final normalizedPrefix = prefix.endsWith('/') ? prefix : '$prefix/';
-  return manifest
-      .listAssets()
+  return assets
       .where(
         (asset) =>
             asset.startsWith(normalizedPrefix) &&
--- test/widgets/managed_ad_placeholder_ticker_support_test.dart
+++ test/widgets/managed_ad_placeholder_ticker_support_test.dart
@@ -5,6 +5,47 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:presto_app/widgets/managed_ad_placeholder_ticker_support.dart';
 
 void main() {
+  group('loadManagedAdFallbackAssets', () {
+    test('normalise le préfixe, filtre les images et trie les assets', () async {
+      var calls = 0;
+
+      final assets = await loadManagedAdFallbackAssets(
+        'assets/carousel_home',
+        manifestEntriesLoader: () async {
+          calls++;
+          return <String>[
+            'assets/carousel_home/z.webp',
+            'assets/carousel_home/readme.txt',
+            'assets/other/ignored.png',
+            'assets/carousel_home/a.JPG',
+            'assets/carousel_home/m.jpeg',
+            'assets/carousel_home/b.PNG',
+          ];
+        },
+      );
+
+      expect(calls, 1);
+      expect(assets, <String>[
+        'assets/carousel_home/a.JPG',
+        'assets/carousel_home/b.PNG',
+        'assets/carousel_home/m.jpeg',
+        'assets/carousel_home/z.webp',
+      ]);
+    });
+
+    test('conserve un préfixe déjà terminé par une barre oblique', () async {
+      final assets = await loadManagedAdFallbackAssets(
+        'assets/banner/',
+        manifestEntriesLoader: () async => <String>[
+          'assets/banner/image.jpg',
+          'assets/banner_extra/image.jpg',
+        ],
+      );
+
+      expect(assets, <String>['assets/banner/image.jpg']);
+    });
+  });
+
   group('ManagedAdBannerImageSource', () {
     test('crée une source asset avec une clé stable et AssetImage', () {
       final source = ManagedAdBannerImageSource.asset(
