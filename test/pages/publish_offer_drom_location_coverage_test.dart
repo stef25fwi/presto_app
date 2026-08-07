0f4963ddd610870e31a509619ba48e647dfe93b6
stef25fwi <sahai.stephane@gmail.com>
2026-08-07 10:57:02 -0400

    refactor(lot2): découpler le test localisation publication

--- test/pages/publish_offer_drom_location_coverage_test.dart
+++ test/pages/publish_offer_drom_location_coverage_test.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core_platform_interface/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:presto_app/features/offers/presentation/widgets/publish_offer_contact_fields.dart';
-import 'package:presto_app/main.dart' as app;
+import 'package:presto_app/pages/publish_offer_page.dart';
 import 'package:presto_app/widgets/ai_publish_control.dart';
 import 'package:presto_app/widgets/city_postal_autocomplete_field.dart';
 
@@ -22,7 +22,7 @@ void main() {
     addTearDown(tester.view.resetDevicePixelRatio);
 
     await tester.pumpWidget(
-      const MaterialApp(home: app.PublishOfferPage()),
+      const MaterialApp(home: PublishOfferPage()),
     );
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 250));
