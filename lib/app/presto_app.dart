f5124a7eddacd5323a835dff773bc2e2477b169f
stef25fwi <sahai.stephane@gmail.com>
2026-08-12 07:44:08 -0400

    isolate prelaunch legal shell

--- lib/app/presto_app.dart
+++ lib/app/presto_app.dart
@@ -11,8 +11,12 @@ import 'theme.dart';
 import '../core/connectivity/connectivity_status.dart';
 import '../core/localization/locale_controller.dart';
 import '../l10n/app_localizations.dart';
+import '../pages/legal_info_page.dart';
+import '../pages/public_prelaunch_page.dart';
+import '../platform/public_prelaunch_shell.dart';
 import '../services/app_check_bootstrap.dart';
 import '../services/notification_service.dart';
+import '../services/public_landing_config_service.dart';
 import '../widgets/admin_web_debug_panel.dart';
 import '../widgets/app_shell_widgets.dart';
 
@@ -65,7 +69,9 @@ class _PrestoAppState extends State<PrestoApp> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     return ListenableBuilder(
       listenable: LocaleController.instance,
-      builder: (context, _) => _buildMaterialApp(),
+      builder: (context, _) => _PublicPrelaunchEntryGate(
+        applicationBuilder: _buildMaterialApp,
+      ),
     );
   }
 
@@ -96,5 +102,134 @@ class _PrestoAppState extends State<PrestoApp> with WidgetsBindingObserver {
       theme: buildPrestoTheme(),
       home: _routes.buildInitialHome(),
     );
+  }
+}
+
+enum PublicPrelaunchEntryMode {
+  application,
+  landing,
+  legalNotices,
+  terms,
+}
+
+@visibleForTesting
+PublicPrelaunchEntryMode resolvePublicPrelaunchEntryMode(
+  Uri uri, {
+  required bool enabled,
+  required bool hasDeveloperAccess,
+  bool isWeb = kIsWeb,
+}) {
+  if (!isWeb || !enabled || hasDeveloperAccess) {
+    return PublicPrelaunchEntryMode.application;
+  }
+
+  const publicHosts = <String>{
+    'ilipresto.fr',
+    'www.ilipresto.fr',
+    'ilipresto.web.app',
+    'ilipresto.firebaseapp.com',
+    'presto-app-74abe.web.app',
+    'presto-app-74abe.firebaseapp.com',
+  };
+  if (!publicHosts.contains(uri.host.trim().toLowerCase())) {
+    return PublicPrelaunchEntryMode.application;
+  }
+
+  var path = uri.path.trim();
+  if (path.isEmpty) path = '/';
+  if (!path.startsWith('/')) path = '/$path';
+  if (path.length > 1 && path.endsWith('/')) {
+    path = path.substring(0, path.length - 1);
+  }
+
+  if (path == LegalInfoPage.legalNoticesRouteName) {
+    return PublicPrelaunchEntryMode.legalNotices;
+  }
+  if (path == LegalInfoPage.termsRouteName) {
+    return PublicPrelaunchEntryMode.terms;
+  }
+
+  return PublicPrelaunchEntryMode.landing;
+}
+
+class _PublicPrelaunchEntryGate extends StatefulWidget {
+  const _PublicPrelaunchEntryGate({required this.applicationBuilder});
+
+  final Widget Function() applicationBuilder;
+
+  @override
+  State<_PublicPrelaunchEntryGate> createState() =>
+      _PublicPrelaunchEntryGateState();
+}
+
+class _PublicPrelaunchEntryGateState extends State<_PublicPrelaunchEntryGate> {
+  final PublicLandingConfigService _publicLanding =
+      PublicLandingConfigService.instance;
+
+  bool _temporaryDeveloperAccessGranted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _publicLanding.addListener(_handleConfigChanged);
+    unawaited(_publicLanding.initialize());
+  }
+
+  @override
+  void dispose() {
+    _publicLanding.removeListener(_handleConfigChanged);
+    super.dispose();
+  }
+
+  void _handleConfigChanged() {
+    if (mounted) setState(() {});
+  }
+
+  void _grantDeveloperAccess() {
+    if (_temporaryDeveloperAccessGranted) return;
+    _temporaryDeveloperAccessGranted = true;
+    revealApplicationAfterPublicPrelaunch();
+    if (mounted) setState(() {});
+  }
+
+  MaterialApp _legalShell({required int initialTab}) {
+    return MaterialApp(
+      title: 'iliprestō',
+      debugShowCheckedModeBanner: false,
+      theme: buildPrestoTheme(),
+      home: LegalInfoPage(
+        initialTab: initialTab,
+        restrictToPrelaunchLegalTabs: true,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mode = resolvePublicPrelaunchEntryMode(
+      Uri.base,
+      enabled: _publicLanding.enabled,
+      hasDeveloperAccess:
+          _temporaryDeveloperAccessGranted || hasPublicPrelaunchAccess(),
+    );
+
+    switch (mode) {
+      case PublicPrelaunchEntryMode.application:
+        return widget.applicationBuilder();
+      case PublicPrelaunchEntryMode.legalNotices:
+        return _legalShell(initialTab: 0);
+      case PublicPrelaunchEntryMode.terms:
+        return _legalShell(initialTab: 2);
+      case PublicPrelaunchEntryMode.landing:
+        return MaterialApp(
+          title: 'iliprestō',
+          debugShowCheckedModeBanner: false,
+          theme: buildPrestoTheme(),
+          home: PublicPrelaunchPage(
+            config: _publicLanding,
+            onDeveloperAccessGranted: _grantDeveloperAccess,
+          ),
+        );
+    }
   }
 }
